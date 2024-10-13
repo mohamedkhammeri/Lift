678f2c73e22ceff48bff28fbe3eb6e1223f713b2
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2024-10-13 19:19:58 +0100

    Initial commit of Spring Boot Lift project

---
+++ src/test/java/com/project/Lift/LiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Lift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
